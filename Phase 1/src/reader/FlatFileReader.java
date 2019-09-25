9bc3772820d27704f1ae841cedc49f30f89db3f1
Joel <joel8019@yahoo.com>
2019-09-25 17:19:20 -0500

    Edits 2

--- Phase 1/src/reader/FlatFileReader.java
+++ Phase 1/src/reader/FlatFileReader.java
@@ -1,9 +1,31 @@
 package reader;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import entities.Person;
+import entities.Customer;
+
 public class FlatFileReader {
 
-	public FlatFileReader() {
-		// TODO Auto-generated constructor stub
+	public  void fileReader() {
+		Scanner sc = null;
+		
+		
+		try {
+			sc = new Scanner (new File("data/Customers.Dat"));
+			sc.nextLine();
+			
+			ArrayList<Person> personList = new ArrayList<Person>();
+			ArrayList<Customer> customerList = new ArrayList<Customer>();
+			while(sc.hasNext()) {
+				String line = sc.nextLine();
+				String data[] = line.split(";");
+				
+			}
+		}
+		
 	}
 
 }
