ede531f5830325d4f515e87da86844c52997f9d9
Joel <joel8019@yahoo.com>
2019-09-25 14:28:32 -0500

    My edits

--- Phase 1/src/entities/Person.java
+++ Phase 1/src/entities/Person.java
@@ -1,9 +1,53 @@
 package entities;
 
+import java.util.ArrayList;
+
 public class Person {
 
-	public Person() {
-		// TODO Auto-generated constructor stub
+	private String personCode;
+	private String firstName;
+	private String lastName;
+	private Address address;
+	private ArrayList<String> email;
+	public Person(String personCode, String firstName, String lastName, Address address, ArrayList<String> email) {
+		super();
+		this.personCode = personCode;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.email = email;
 	}
+	public String getPersonCode() {
+		return personCode;
+	}
+	public void setPersonCode(String personCode) {
+		this.personCode = personCode;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public ArrayList<String> getEmail() {
+		return email;
+	}
+	public void setEmail(ArrayList<String> email) {
+		this.email = email;
+	}
+	
+	
 
 }
